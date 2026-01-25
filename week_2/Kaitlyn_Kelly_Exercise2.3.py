82d897a024db4e2d7708e56a4ace913db1cc4343
kaitlynnnkay <ecommbykaitlyn@gmail.com>
2026-01-25 11:12:54 -0600

    add rest of week 2 files

---
+++ week_2/Kaitlyn_Kelly_Exercise2.3.py
@@ -0,0 +1,8 @@
+"""
+Author: Kaitlyn Kelly
+Date: 1/25/2026
+File Name: TBD
+Description: Exercise 2.3 - Python
+"""
+# Print the quote and credit the author
+print("'You know, my favorite thing about the movie is, like, it feels like a movie.' - Harry Styles")
