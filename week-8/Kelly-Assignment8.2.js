2830ce1e67a034ecfccfd0c4065f40a450b3745d
kaitlynnnkay <ecommbykaitlyn@gmail.com>
2026-05-17 14:27:18 -0500

    add week 9 files

--- week-8/Kelly-Assignment8.2.js
+++ week-8/Kelly-Assignment8.2.js
@@ -16,3 +16,12 @@ db.books.find({author: "Karin Slaughter"})
 
 // display a book by bookId
 db.books.findOne({bookId: "003"})
+
+// display a wishlist by customerId
+db.customers.findOne({customerId: "1"},{wishlistitems: 1, _id: 0})
+
+// add books to a customer's wishlist
+db.customers.updateOne({customerId: "1"},{$addToSet: {wishlistitems: "002"}})
+
+// remove book from a customer's wishlist
+db.customers.updateOne({customerId: "1"},{$pull: {wishlistitems: "002"}})
